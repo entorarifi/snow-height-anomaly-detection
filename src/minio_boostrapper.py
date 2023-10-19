c969b853d251fe707c62a07e4e93b7d0c6f9c0b8
Entor Arifi <entor.arifi@jaywalker.ch>
2023-10-19 22:57:03 +0200

    Updated minio & mlflow configuration.

--- src/minio_boostrapper.py
+++ src/minio_boostrapper.py
@@ -6,8 +6,9 @@ from utils import setup_logger
 
 
 class MinioClientBootstrapper:
-    def __init__(self, endpoint, port, access_key, secret_key, secure=False):
+    def __init__(self, endpoint, port, access_key, secret_key, secure, mlflow_bucket):
         self.client = Minio(f"{endpoint}:{port}", access_key, secret_key, secure=secure)
+        self.mlflow_bucket = mlflow_bucket
 
     def create_bucket_if_not_exists(self, bucket_name):
         if not self.client.bucket_exists(bucket_name):
@@ -17,7 +18,7 @@ class MinioClientBootstrapper:
             logging.info(f"Bucket '{bucket_name}' already exists.")
 
     def bootstrap_buckets(self):
-        self.create_bucket_if_not_exists("mlflow")
+        self.create_bucket_if_not_exists(self.mlflow_bucket)
         self.create_bucket_if_not_exists("data")
 
 
@@ -30,13 +31,15 @@ if __name__ == '__main__':
     ACCESS_KEY = os.getenv('MINIO_ROOT_USER')
     SECRET_KEY = os.getenv('MINIO_ROOT_PASSWORD')
     SECURE = os.getenv('MINIO_STORAGE_USE_HTTPS').lower() == True
+    MLFLOW_BUCKET_NAME = os.getenv('MLFLOW_BUCKET_NAME')
 
     minio_bootstrapper = MinioClientBootstrapper(
         endpoint=URL,
         port=PORT,
         access_key=ACCESS_KEY,
         secret_key=SECRET_KEY,
-        secure=SECURE
+        secure=SECURE,
+        mlflow_bucket=MLFLOW_BUCKET_NAME
     )
 
     minio_bootstrapper.bootstrap_buckets()
